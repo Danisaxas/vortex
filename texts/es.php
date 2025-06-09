d6da967ee30c7e5928900fbe50cbc76266fd5bd1
Danisaxas <danluismay@gmail.com>
2025-06-08 22:30:12 -0400

    hfg

--- texts/es.php
+++ texts/es.php
@@ -5,5 +5,7 @@ return [
 
 [🇪🇸] Hola @{username} Bienvenido a {bot_name} Telegram Bot, las puertas de enlace, las herramientas y las funciones se agregan constantemente, para saber que mis diferentes comandos usan los botones que se muestran aquí:
 ━━━━━━━━━━━━━
-<code>Api Bot El estado es: Online ✅ | {bot_name} Api ¡Está en línea!!</code>"
+<code>Api Bot El estado es: Online ✅ | {bot_name} Api ¡Está en línea!!</code>",
+
+'id_txt'    => "Tu ID: {user_id}\nUsuario: @{username}"
 ];
